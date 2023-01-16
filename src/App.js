8c1d44be94e303d6c33abcf3a8723cb068632dcd
Caden Huffman <you@example.com>
2023-01-16 00:02:34 -0500

    Working on Board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+function Square(){
+  return(
+    <button className = "square">
+      
+    </button>
   );
+
 }
 
-export default App;
+
+function Board(){
+  return(
+    <div className = "board">
+      <table>
+          <tr>
+            <td><Square/></td>
+            <td><Square/></td>
+            <td><Square/></td>
+          </tr>
+          <tr>
+            <td><Square/></td>
+            <td><Square/></td>
+            <td><Square/></td>
+
+          </tr>
+          <tr>
+            <td><Square/></td>
+            <td><Square/></td>
+            <td><Square/></td>
+          </tr>
+      </table>
+    </div>
+  );
+  
+
+}
+
+
+export default function App(){
+  return(
+    <Board />
+  );
+
+
+
+}
